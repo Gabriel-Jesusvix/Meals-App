7643984559a60f16f60b939951c2a25fbc1d6564
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-17 16:39:11 -0300

    feat: add translate title categories

--- src/mocks/dummy-data.ts
+++ src/mocks/dummy-data.ts
@@ -4,12 +4,12 @@ import { Meal } from "./models/meal";
 export const CATEGORIES = [
   new Category({
     id: 'c1',
-    title: 'Italian',
+    title: 'Italiana - Italian',
     color: '#f5428d',
   }),
   new Category({
     id: 'c2',
-    title: 'Quick & Easy',
+    title: 'Rápido e fácil - Quick & Easy',
     color: '#f54242',
   }),
   new Category({
@@ -19,37 +19,37 @@ export const CATEGORIES = [
   }),
   new Category({
     id: 'c4',
-    title: 'German',
+    title: 'Alemã - German',
     color: '#f5d142',
   }),
   new Category({
     id: 'c5',
-    title: 'Light & Lovely',
+    title: 'Leve e adorável - Light & Lovely',
     color: '#368dff',
   }),
   new Category({
     id: 'c6',
-    title: 'Exotic',
+    title: ' Exótica - Exotic',
     color: '#41d95d',
   }),
   new Category({
     id: 'c7',
-    title: 'Breakfast',
+    title: 'Café da manhã - Breakfast',
     color: '#9eecff',
   }),
   new Category({
     id: 'c8',
-    title: 'Asian',
+    title: 'Asiática - Asian',
     color: '#b9ffb0',
   }),
   new Category({
     id: 'c9',
-    title: 'French',
+    title: 'Francesa - French',
     color: '#ffc7ff',
   }),
   new Category({
     id: 'c10',
-    title: 'Summer',
+    title: 'Verão - Summer',
     color: '#47fced',
   }),
 ];
