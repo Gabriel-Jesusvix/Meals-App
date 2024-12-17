2bf5358dfe1e7232c3c1211c5521fd24c6ac38f5
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-17 20:56:32 -0300

    refactor: introduce Main app with appRoutes

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,14 +1,12 @@
-import { SafeAreaView, StatusBar } from 'react-native';
-import { s } from './styles';
-import { Categories } from '../screens/Categories';
+import { StatusBar } from 'react-native';
 import { AppRoutes } from '../routes';
 
 export function Main() {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: "#24180f"}}>
-      <StatusBar animated barStyle={'light-content'} showHideTransition={'fade'} translucent={false} />
+    <>
+      <StatusBar animated barStyle={'dark-content'} showHideTransition={'fade'} translucent={false} />
       <AppRoutes />
-    </SafeAreaView>
+    </>
   );
 }
 
