6a2cd672d184c742a7849ec3dd528d41a7c33c6b
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-17 10:43:19 -0300

    chore: setup project

---
+++ src/App/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { s } from './styles';
+
+export  function Main() {
+  return (
+    <View style={s.container}>
+      <Text>Point initial</Text>
+    </View>
+  );
+}
+
