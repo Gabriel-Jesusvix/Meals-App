8b9c3f0e8a1f2bbf8b4f552dc34025c2e013ce7c
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-18 15:25:19 -0300

    feat: receive parameter to navigate screen

--- src/screens/MealsOverview/index.tsx
+++ src/screens/MealsOverview/index.tsx
@@ -1,10 +1,17 @@
+import { useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
 
+type RouteParamProps = {
+  id: string;
+}
 
 export function MealsOverview() {
+  const route = useRoute();
+  const { id } = route.params as RouteParamProps;
+
   return (
     <View>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen: ${id}</Text>
     </View>
   )
 }
