4012e6316c87cf1050575ef4411b6d39726ed3ab
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-18 16:39:59 -0300

    remove: safe area component

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,11 @@
-import { SafeAreaView, StatusBar, View } from 'react-native';
+import {  StatusBar, View } from 'react-native';
 import { AppRoutes } from '../routes';
 
 export function Main() {
   return (
     <>
       <StatusBar animated barStyle={'dark-content'} showHideTransition={'fade'} translucent={false} />
-      <SafeAreaView style={{ flex: 1 }}>
-        <AppRoutes />
-      </SafeAreaView>
+      <AppRoutes />
     </>
   );
 }
