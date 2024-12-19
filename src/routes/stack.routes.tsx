c911def209c825178b6095cb6ce69d094aff3623
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-19 16:59:20 -0300

    fix: remove spacing

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -34,7 +34,6 @@ export function Routes() {
         <Screen 
           name='MealsDetails'
           component={MealsDetails}
-          
         />
       </Navigator>
     </NavigationContainer>
