e5d64d7b78ab1d2f10541aaea405818a55eb53f4
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-20 00:11:34 -0300

    feat: add navigation container on the main app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { Main } from './src/App';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <Main />
-    </>
+    </NavigationContainer>
   );
 }
 
