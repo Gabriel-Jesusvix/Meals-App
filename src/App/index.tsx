266df11c531987eecfe32ba96e6eb2669b43b5cb
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-20 00:27:06 -0300

    feat: wrapper context Provider on the main app

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,13 @@
 import {  StatusBar, View } from 'react-native';
 import { AppRoutes } from '../routes';
+import { FavoriteContextProvider } from '../store/context/FavoriteContext';
 
 export function Main() {
   return (
-    <>
+    <FavoriteContextProvider>
       <StatusBar animated barStyle={'light-content'} showHideTransition={'fade'} translucent={false} />
       <AppRoutes />
-    </>
+    </FavoriteContextProvider>
   );
 }
 
