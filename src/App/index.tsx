11ec793c2aca90f972606cdf658c5381601f3a80
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-20 01:19:56 -0300

    feat: create store with redux toolkit

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,19 @@
-import {  StatusBar, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { AppRoutes } from '../routes';
 import { FavoriteContextProvider } from '../store/context/FavoriteContext';
+import { Provider } from 'react-redux';
+import { store } from '../store/redux/store';
 
 export function Main() {
   return (
-    <FavoriteContextProvider>
-      <StatusBar animated barStyle={'light-content'} showHideTransition={'fade'} translucent={false} />
-      <AppRoutes />
-    </FavoriteContextProvider>
+    <Provider
+      store={store}
+    >
+      <FavoriteContextProvider>
+        <StatusBar animated barStyle={'light-content'} showHideTransition={'fade'} translucent={false} />
+        <AppRoutes />
+      </FavoriteContextProvider>
+    </Provider>
   );
 }
 
