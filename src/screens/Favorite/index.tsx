2be7211bd774fa8e0e0ec31427c8650cb3192b09
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2024-12-20 01:00:23 -0300

    feat: create screen Favorite

--- src/screens/Favorite/index.tsx
+++ src/screens/Favorite/index.tsx
@@ -1,10 +1,27 @@
 import { Text, View } from "react-native";
+import { s } from "./styles";
+import { useFavorite } from "../../store/context/hooks/useFavorite";
+import { MEALS } from "../../mocks/dummy-data";
+import { MealsList } from "./components/MealsList";
 
 
 export function Favorite() {
+  const { ids } = useFavorite()
+  const favoriteMeals = MEALS.filter((meal) =>
+    ids.includes(meal.getId())
+  );
+  console.log("favoriteMeals",favoriteMeals)
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={s.rootContainer}>
+        <Text style={s.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Favorite Screen</Text>
-    </View>
+    <MealsList items={favoriteMeals} />
   )
+
 }
